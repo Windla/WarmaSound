32b31e52fbbdb65e51a5d10eca94c97ee590e38b
Windla <63698964+Windla@users.noreply.github.com>
2022-02-19 14:54:43 +0800

    v1.1

---
+++ _del.bat
@@ -0,0 +1,23 @@
+::这是一个用于删除与原皮肤冲突音效的bat脚本
+::涉及 ".mp3" ".ogg" ".wav" 请注意备份
+
+::作者主页: https://space.bilibili.com/358002685
+::发布地址: https://github.com/Windla/WarmaSound
+
+
+del /s /q "applause.*"
+del /s /q "check-on.*"
+del /s /q "check-off.*"
+del /s /q "combobreak.*"
+del /s /q "count*.*"
+del /s /q "failsound.*"
+del /s /q "gos.*"
+del /s /q "match-join.*"
+del /s /q "menuhit.*"
+del /s /q "menuback.*"
+del /s /q "pause-loop.*"
+del /s /q "readys.*"
+del /s /q "sectionfail.*"
+del /s /q "sectionpass.*"
+del /s /q "shutter.*"
+del /s /q "spinnerspin.*"
