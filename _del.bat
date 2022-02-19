8ca52e1eafc6c314da6856c36b98c6fffab2c89c
Windla <63698964+Windla@users.noreply.github.com>
2022-02-19 14:59:00 +0800

    Add files via upload

--- _del.bat
+++ _del.bat
@@ -1,6 +1,10 @@
 ::这是一个用于删除与原皮肤冲突音效的bat脚本
 ::涉及 ".mp3" ".ogg" ".wav" 请注意备份
 
+::使用方法:
+::将 "_del.bat" 移动到要替换的皮肤文件夹内
+::运行, 随后将 "Warma Sound" 所有音频文件复制过去
+
 ::作者主页: https://space.bilibili.com/358002685
 ::发布地址: https://github.com/Windla/WarmaSound
 
